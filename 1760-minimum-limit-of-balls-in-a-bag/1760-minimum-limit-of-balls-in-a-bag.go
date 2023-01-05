9d2acc63a0b8061e5a029c04dff5a9b757aca8f1
hadooboo <32262002+hadooboo@users.noreply.github.com>
2023-01-06 01:13:41 +0900

    Time: 492 ms (6.67%), Space: 10.3 MB (26.67%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.go
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.go
@@ -1,13 +1,17 @@
 import "sort"
 
 func minimumSize(nums []int, maxOperations int) int {
+	sort.Ints(nums)
 	return sort.Search(10e9, func(i int) bool {
 		if i == 0 {
 			return false
 		}
 		c := 0
-		for _, v := range nums {
-			c += (v+i-1)/i - 1
+		for j := len(nums) - 1; j >= 0; j-- {
+			if i >= nums[j] {
+				break
+			}
+			c += (nums[j]+i-1)/i - 1
 		}
 		return c <= maxOperations
 	})
