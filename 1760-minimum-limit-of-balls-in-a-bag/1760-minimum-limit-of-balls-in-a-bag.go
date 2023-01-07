529b7540722e2634a53dac4b7e04a006df0deb1a
hadooboo <32262002+hadooboo@users.noreply.github.com>
2023-01-08 02:49:25 +0900

    Time: 217 ms (56.25%), Space: 8.8 MB (56.25%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.go
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.go
@@ -1,10 +1,8 @@
 import (
 	"math"
-	"sort"
 )
 
 func minimumSize(nums []int, maxOperations int) int {
-	sort.Ints(nums)
 	max, sum := 0, 0
 	for _, v := range nums {
 		if max < v {
@@ -13,15 +11,25 @@ func minimumSize(nums []int, maxOperations int) int {
 		sum += v
 	}
 	min := int(math.Ceil(float64(sum) / float64(maxOperations+len(nums))))
-	return sort.Search(max-min+1, func(i int) bool {
+	return doBinarySearch(max-min+1, func(i int) bool {
 		i += min
 		c := 0
-		for j := len(nums) - 1; j >= 0; j-- {
-			if i >= nums[j] {
-				break
-			}
-			c += (nums[j]+i-1)/i - 1
+		for _, v := range nums {
+			c += (v+i-1)/i - 1
 		}
 		return c <= maxOperations
 	}) + min
+}
+
+func doBinarySearch(n int, f func(int) bool) int {
+	i, j := 0, n
+	for i < j {
+		h := int(uint(i+j) >> 1)
+		if !f(h) {
+			i = h + 1
+		} else {
+			j = h
+		}
+	}
+	return i
 }
